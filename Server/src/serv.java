3314f90f1df8863a41b1a1d7e5c75934e1b90c4d
Saldor010 <saldor10@gmail.com>
2020-03-26 18:27:17 -0500

    HTTP Server Update
    This update expands the HTTP server code to serve the front end to the user as well as respond to a specific ping request from the front end.

--- Server/src/serv.java
+++ Server/src/serv.java
@@ -5,21 +5,121 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class serv {
-
+    private static String indexHTML;
+    private static String adminPanelHTML;
+    private static String merchantPanelHTML;
+    private static String shoppingCartHTML;
+    private static String stylesheetCSS;
+    
     public static void main(String[] args) throws Exception {
+        InputStream in1 = serv.class.getResourceAsStream("index.html");
+        indexHTML = new BufferedReader(new InputStreamReader(in1)).lines().collect(Collectors.joining("\n"));
+        
+        InputStream in2 = serv.class.getResourceAsStream("adminPanel.html");
+        adminPanelHTML = new BufferedReader(new InputStreamReader(in2)).lines().collect(Collectors.joining("\n"));
+        
+        InputStream in3 = serv.class.getResourceAsStream("merchantPanel.html");
+        merchantPanelHTML = new BufferedReader(new InputStreamReader(in3)).lines().collect(Collectors.joining("\n"));
+        
+        InputStream in4 = serv.class.getResourceAsStream("shoppingCart.html");
+        shoppingCartHTML = new BufferedReader(new InputStreamReader(in4)).lines().collect(Collectors.joining("\n"));
+        
+        InputStream in5 = serv.class.getResourceAsStream("stylesheet.css");
+        stylesheetCSS = new BufferedReader(new InputStreamReader(in5)).lines().collect(Collectors.joining("\n"));
+        
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/ping", new MyHandler());
+        server.createContext("/index.html", new indexHandler());
+        server.createContext("/adminPanel.html", new adminHandler());
+        server.createContext("/merchantPanel.html", new merchantHandler());
+        server.createContext("/shoppingCart.html", new shoppingHandler());
+        server.createContext("/stylesheet.css", new styleHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class indexHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            byte [] response = "Test Page".getBytes();
+            byte[] response = indexHTML.getBytes();
+            
             t.sendResponseHeaders(200, response.length);
             OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class adminHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte[] response = adminPanelHTML.getBytes();
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class merchantHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte[] response = merchantPanelHTML.getBytes();
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class shoppingHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte[] response = shoppingCartHTML.getBytes();
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class styleHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte[] response = stylesheetCSS.getBytes();
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte [] response = "Pong!".getBytes();
+            //byte[] response = indexHTML.getBytes();
+            
+            StringBuilder sb = new StringBuilder();
+            InputStream argumentStream = t.getRequestBody();
+            int i;
+            while((i = argumentStream.read()) != -1) {
+                sb.append((char) i);
+            }
+            System.out.println("aaa: " + sb.toString());
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
             os.write(response);
             os.close();
         }
