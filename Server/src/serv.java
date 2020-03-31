c2738497c2384aac98cf6edb48e18be222ee82a7
Saldor010 <saldor10@gmail.com>
2020-03-30 21:17:23 -0500

    SMS Update
    I've updated the frontend and backend to support sending SMS codes to the customer. For the Java portion, I have used Konnor's SMS generator for the bulk of the work. It's currently set to go through a random gmail account I set up, so it should work without you guys having to put in your own email.

--- Server/src/serv.java
+++ Server/src/serv.java
@@ -1,3 +1,5 @@
+package softwareengineering;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -8,6 +10,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class serv {
@@ -17,24 +21,41 @@ public class serv {
     private static String shoppingCartHTML;
     private static String stylesheetCSS;
     
+    // https://stackoverflow.com/questions/11640025/how-to-obtain-the-query-string-in-a-get-with-java-httpserver-httpexchange
+    public static Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
+    
     public static void main(String[] args) throws Exception {
-        InputStream in1 = serv.class.getResourceAsStream("index.html");
+        InputStream in1 = SoftwareEngineering.class.getResourceAsStream("index.html");
         indexHTML = new BufferedReader(new InputStreamReader(in1)).lines().collect(Collectors.joining("\n"));
         
-        InputStream in2 = serv.class.getResourceAsStream("adminPanel.html");
+        InputStream in2 = SoftwareEngineering.class.getResourceAsStream("adminPanel.html");
         adminPanelHTML = new BufferedReader(new InputStreamReader(in2)).lines().collect(Collectors.joining("\n"));
         
-        InputStream in3 = serv.class.getResourceAsStream("merchantPanel.html");
+        InputStream in3 = SoftwareEngineering.class.getResourceAsStream("merchantPanel.html");
         merchantPanelHTML = new BufferedReader(new InputStreamReader(in3)).lines().collect(Collectors.joining("\n"));
         
-        InputStream in4 = serv.class.getResourceAsStream("shoppingCart.html");
+        InputStream in4 = SoftwareEngineering.class.getResourceAsStream("shoppingCart.html");
         shoppingCartHTML = new BufferedReader(new InputStreamReader(in4)).lines().collect(Collectors.joining("\n"));
         
-        InputStream in5 = serv.class.getResourceAsStream("stylesheet.css");
+        InputStream in5 = SoftwareEngineering.class.getResourceAsStream("stylesheet.css");
         stylesheetCSS = new BufferedReader(new InputStreamReader(in5)).lines().collect(Collectors.joining("\n"));
         
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/ping", new MyHandler());
+        
+        server.createContext("/ping", new pingHandler());
+        server.createContext("/sendsms", new SMSHandler());
+        
         server.createContext("/index.html", new indexHandler());
         server.createContext("/adminPanel.html", new adminHandler());
         server.createContext("/merchantPanel.html", new merchantHandler());
@@ -104,7 +125,7 @@ public class serv {
         }
     }
     
-    static class MyHandler implements HttpHandler {
+    static class pingHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
             byte [] response = "Pong!".getBytes();
             //byte[] response = indexHTML.getBytes();
@@ -116,6 +137,31 @@ public class serv {
                 sb.append((char) i);
             }
             System.out.println("aaa: " + sb.toString());
+            
+            t.sendResponseHeaders(200, response.length);
+            OutputStream os = t.getResponseBody();
+            
+            os.write(response);
+            os.close();
+        }
+    }
+    
+    static class SMSHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            byte [] response = "Pong!".getBytes();
+            //byte[] response = indexHTML.getBytes();
+            
+            Map<String,String> arguments = queryToMap(t.getRequestURI().getQuery());
+            if(arguments.containsKey("phoneNumber") && arguments.containsKey("carrier")) {
+                System.out.println("Attempting to send SMS..");
+                int securityCode = textsend.sendSMS(arguments.get("phoneNumber"), arguments.get("carrier"));
+                if(securityCode > 0) {
+                    response = String.valueOf(securityCode).getBytes();
+                }
+            } else {
+                System.out.println("Malformed SMS request!");
+                response = "Malformed request!".getBytes();
+            }
             
             t.sendResponseHeaders(200, response.length);
             OutputStream os = t.getResponseBody();
--- Server/src/textsend.java
+++ Server/src/textsend.java
@@ -1,4 +1,4 @@
-package serv;
+package softwareengineering;
 
 // JavaMail
 import javax.mail.*;
@@ -9,28 +9,49 @@ import java.util.Random;
 
 public class textsend {
 
-    public static void main(String[] args) {
+    public static int sendSMS(String phoneNumber, String carrier) {
         //Random number generator for customer code
         Random rand = new Random();
         int customercode = 1000 + rand.nextInt(1000);
         //Email and password of account sending email
-        final String username = "xxxx";
-        final String password = "xxxx";
+        final String username = "helloWorld4949";
+        final String password = "6MQcQEL2Ccgw6SZ";
 
         //Phone Carriers
         final String tmobile = "@tmomail.net";
         final String sprint = "@messaging.sprintpcs.com";
         final String verizon = "@vtext.com";
         final String att = "@txt.att.net";
+        
+        final String customerCarrier;
+        
+        switch(carrier) {
+            case "tmobile":
+                customerCarrier = tmobile;
+                break;
+            case "sprint":
+                customerCarrier = sprint;
+                break;
+            case "verizon":
+                customerCarrier = verizon;
+                break;
+            case "att":
+                customerCarrier = att;
+                break;
+            default:
+                System.out.println(carrier + " is not a valid carrier!");
+                return -1;
+        }
 
         // Phone number you want to send message too
-        String phonenumber = "xxxxxxxxxx";
+        String phonenumber = phoneNumber;
 
         Properties prop = new Properties();
         //mail server properties
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         prop.put("mail.smtp.starttls.enable", "true");
 
         //Authenticate login for email server
@@ -43,11 +64,12 @@ public class textsend {
         });
 
         try {
-
+            System.out.println("Sending SMS to "+phonenumber+" on carrier "+carrier);
+            
             Message message = new MimeMessage(session);
             message.setRecipients(
                     Message.RecipientType.TO,
-                    InternetAddress.parse(phonenumber + sprint)
+                    InternetAddress.parse(phonenumber + customerCarrier)
             );
             //Email Subject
             message.setSubject("Customer Security Code");
@@ -59,7 +81,10 @@ public class textsend {
             System.out.println("Text Sent!");
 
         } catch (MessagingException e) {
+            e.printStackTrace();
         }
+        
+        return customercode;
     }
 
 }
